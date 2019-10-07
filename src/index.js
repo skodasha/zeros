0b8aca3461c1a43e0546eae472d2b35257faa2b3
skodasha <skodasha@users.noreply.github.com>
2019-10-07 14:50:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let numbers = expression.split("*");
+  let num;
+  let result = [];
+  let count;
+  let countZeros = 0;
+
+  for(let i = 0; i < numbers.length;i++){
+    if(numbers[i].slice(-2) == "!!"){
+      num = BigInt(numbers[i].slice(0,-2));
+      count = BigInt(2);   
+    }
+    else {
+      num = BigInt(numbers[i].slice(0,-1));
+      count = BigInt(1);
+    }
+    result[i] = num;
+    num = num - count;
+    while(num >= 1){
+      result[i] *= num;
+      num -= count;
+    }
+  }
+  let res = BigInt(1);
+  for(let j = 0; j < result.length; j++){
+    res *= result[j];
+  }
+
+  while( res % 10n === 0n){
+    res = res / 10n;
+    countZeros++;
+  }
+
+  return countZeros;
 }
